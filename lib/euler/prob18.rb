de485d75800dc72fd3b2c439e02bb169e81f55fd
Louise Rains <lrains@berklee.edu>
2014-02-01 09:09:48 -0500

    adds more refactoring

--- lib/euler/prob18.rb
+++ lib/euler/prob18.rb
@@ -4,14 +4,14 @@ module Euler
   class Prob18
     def initialize(filename = 'small_tri.txt')
       @filename = filename
-      @arr_of_lines = []
+      @tri_arr = []
       insert_data_into_array
     end
 
     def insert_data_into_array
       f = File.open(@filename)
       f.each do |line|
-        @arr_of_lines << line.split.map{|n| n.to_i}
+        @tri_arr << line.split.map{|n| n.to_i}
       end
     end
 
@@ -19,17 +19,21 @@ module Euler
       (num_lines-2).downto(0) do |line_to_replace|
         loop_through_line(line_to_replace)
       end
-      @arr_of_lines[0][0]
+      @tri_arr[0][0]
     end
 
     def loop_through_line(current_line)
-      @arr_of_lines[current_line].each_index do |index|
-        @arr_of_lines[current_line][index] = replace_element_with_max_sum(@arr_of_lines[current_line][index],index, current_line)
+      @tri_arr[current_line].each_index do |index|
+        replace_element(current_line,index)
       end
     end
 
+    def replace_element(current_line,index)
+      @tri_arr[current_line][index] = replace_element_with_max_sum(@tri_arr[current_line][index],index, current_line)
+    end
+
     def num_lines
-      @arr_of_lines.length
+      @tri_arr.length
     end
 
     def replace_element_with_max_sum(element,index, n)
@@ -37,8 +41,8 @@ module Euler
     end
 
     def child_value(side,n,index)
-      return @arr_of_lines[n+1][index] if side =='left'
-      @arr_of_lines[n+1][index+1]
+      return @tri_arr[n+1][index] if side =='left'
+      @tri_arr[n+1][index+1]
     end
   end
 end
