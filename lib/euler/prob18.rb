400feb0165dc0c6fd81381a9ad3191d7db1a18da
Louise Rains <lrains@berklee.edu>
2014-02-01 09:06:34 -0500

    refactors prob18

--- lib/euler/prob18.rb
+++ lib/euler/prob18.rb
@@ -16,17 +16,29 @@ module Euler
     end
 
     def find_max_cost
-      num_lines = @arr_of_lines.length
-      (num_lines-2).downto(0) do |n|
-        @arr_of_lines[n].each_index do |index|
-          @arr_of_lines[n][index] = replace_element_with_max_sum(@arr_of_lines[n][index],index, n)
-        end
+      (num_lines-2).downto(0) do |line_to_replace|
+        loop_through_line(line_to_replace)
       end
       @arr_of_lines[0][0]
     end
 
+    def loop_through_line(current_line)
+      @arr_of_lines[current_line].each_index do |index|
+        @arr_of_lines[current_line][index] = replace_element_with_max_sum(@arr_of_lines[current_line][index],index, current_line)
+      end
+    end
+
+    def num_lines
+      @arr_of_lines.length
+    end
+
     def replace_element_with_max_sum(element,index, n)
-      [@arr_of_lines[n+1][index] + element, @arr_of_lines[n+1][index+1] + element].max
+      element += [child_value('left',n,index), child_value('right',n,index)].max
+    end
+
+    def child_value(side,n,index)
+      return @arr_of_lines[n+1][index] if side =='left'
+      @arr_of_lines[n+1][index+1]
     end
   end
 end
